a42e4b6dcf1dd6340e178ce0abab662afb96439c
rkhaidir <rkhaidir085@gmail.com>
2024-10-20 23:24:06 +0800

    slicing template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Layout Default - Mazer Admin Dashboard</title>
+  
+  <link rel="shortcut icon" href="{{ asset('assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
+  
+<link rel="stylesheet" href="{{ asset('assets/compiled/css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/compiled/css/app-dark.css') }}">
+</head>
+
+<body>
+    <script src="{{ asset('assets/static/js/initTheme.js') }}"></script>
+  <div id="app">
+    <div id="sidebar">
+      @include('partials.sidebar')
+    </div>
+    <div id="main">
+      <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+          <i class="bi bi-justify fs-3"></i>
+        </a>
+      </header>
+            
+      <div class="page-heading">
+        <div class="page-title">
+          <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+              <h3>Layout Default</h3>
+              <p class="text-subtitle text-muted">The default layout.</p>
+            </div>
+          </div>
+        </div>
+        <section class="section">
+          
+        </section>
+      </div>
+
+      <footer>
+        <div class="footer clearfix mb-0 text-muted">
+          <div class="float-start">
+            <p>2023 &copy; Mazer</p>
+          </div>
+          <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+              by <a href="https://saugi.me">Saugi</a></p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
+  <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+  <script src="{{ asset('assets/compiled/js/app.js') }}"></script>
+</body>
+
+</html>
